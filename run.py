d8a6d79faec9b7b6037449152ad49d5298d7af57
Andreas Pergantis <andreaspergantis@hotmail.com>
2025-03-06 11:59:02 +0100

    Calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -59,12 +59,12 @@ def validate_data(values):
 def update_worksheet(data, worksheet):
     """
     Receives a list of integers to be inserted into a worksheet
-    Updates the relevant worksheet with the data provided
+    Update the relevant worksheet with the data provided
     """
     print(f"Updating {worksheet} worksheet...\n")
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
-    print(f"{worksheet} worksheet updated successfully.\n")
+    print(f"{worksheet} worksheet updated successfully\n")
 
 
 def calculate_surplus_data(sales_row):
@@ -72,8 +72,8 @@ def calculate_surplus_data(sales_row):
     Compare sales with stock and calculate the surplus for each item type.
 
     The surplus is defined as the sales figure subtracted from the stock:
-    - Positive surplus indicates waste  (stock > sales)
-    - Negative surplus indicates extra made when stock sold out (stock < sales)
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
     """
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
@@ -83,7 +83,7 @@ def calculate_surplus_data(sales_row):
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
-    
+
     return surplus_data
 
 
@@ -91,15 +91,32 @@ def get_last_5_entries_sales():
     """
     Collects columns of data from sales worksheet, collecting
     the last 5 entries for each sandwich and returns the data
-    as a list of lists
+    as a list of lists.
     """
     sales = SHEET.worksheet("sales")
+
     columns = []
     for ind in range(1, 7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
-    
-    return columns   
+
+    return columns
+
+
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
 
 
 def main():
@@ -112,8 +129,9 @@ def main():
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
     sales_columns = get_last_5_entries_sales()
-    pprint(sales_columns)
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
-print("Welcome to Love Sandwiches Data Automation\n")
+print("Welcome to Love Sandwiches Data Automation")
 main()
